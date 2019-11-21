4cc8a1b28b0a7a0d30603c9059e6a81957e444f0
Kevin <kevelberger@gmail.com>
2019-11-21 10:09:12 -0800

    Add react boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # wordrace-ui
 Front end repository for Word Race project
+
+## Installation
+Clone the repository and run the following commands:
+
+```
+npm i && npm start
+```
+
+For production builds run:
+
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'source-map',
+  resolve: { extensions: ['.ts', '.tsx', '.js'] },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "source-map-loader"
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+};
